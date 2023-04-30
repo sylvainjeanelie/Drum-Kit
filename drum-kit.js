9944b3c0349bbb47ddba566781dd8e0a4049ba5c
sylvainjeanelie <sylvainjeanelie@gmail.com>
2023-04-30 23:11:00 +0800

    New-Drum

--- drum-kit.js
+++ drum-kit.js
@@ -1,20 +1,17 @@
-window.addEventListener('keydown', playingSound);
-
 function playingSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
     if(!audio) return;
-   
     audio.play();
     audio.currentTime = 0;
     key.classList.add('playing');
 }
 
-// remove class Plying
 function removeTransition(e){
-if(e.propertyName !== 'transform') return;
-this.classList.remove('playing');
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
 }
 
+window.addEventListener('keydown', playingSound);
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
