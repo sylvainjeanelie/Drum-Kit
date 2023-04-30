a6fd164c85df42310920086d2f75f2f066a6b28f
sylvainjeanelie <sylvainjeanelie@gmail.com>
2023-04-30 23:06:32 +0800

    Second-commit

--- drum-kit.js
+++ drum-kit.js
@@ -1,13 +1,10 @@
-// Add Keydonw Evet
-
 window.addEventListener('keydown', playingSound);
 
 function playingSound(e){
-    // console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
     if(!audio) return;
-    // console.log(audio);
+   
     audio.play();
     audio.currentTime = 0;
     key.classList.add('playing');
